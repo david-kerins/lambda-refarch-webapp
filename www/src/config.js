b3aa1c7b22d29eb7a2a6e4be370f8b42d501aa14
David Kerins <dkerins@bitbox.ca>
2020-12-02 15:02:25 -0800

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "67cl29u7eou09816p52t7fefcp",     // CognitoClientID
+  "api_base_url": "https://pnekqtvb0e.execute-api.ca-central-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-dkerins-lambda-test-1.auth.ca-central-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d10tp9pf1ykcoh.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
